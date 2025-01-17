5e106ec6770127a37b9256b068f0acefd9ec3b61
Leanna Eldridge <leldridg@trinity.edu>
2025-01-17 13:24:55 -0600

    lots of things

---
+++ hello_cargo/target/debug/deps/hello_cargo-e302a534dd2ea05a.d
@@ -0,0 +1,5 @@
+/Users/leann/Code/RustProjects/hello_cargo/target/debug/deps/libhello_cargo-e302a534dd2ea05a.rmeta: src/main.rs
+
+/Users/leann/Code/RustProjects/hello_cargo/target/debug/deps/hello_cargo-e302a534dd2ea05a.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/leann/Code/RustProjects/hello_cargo/target/debug/hello_cargo: /Users/leann/Code/RustProjects/hello_cargo/src/main.rs
